d8011f450c403e8e7392be65b6715213e71d6053
sanchit <sanchit.srivatava@pditechnologies.com>
2023-06-26 23:44:50 +0530

    chore: initial commit

---
+++ android/src/main/java/com/biometricauth/BiometricAuthModule.java
@@ -0,0 +1,32 @@
+package com.biometricauth;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.module.annotations.ReactModule;
+
+@ReactModule(name = BiometricAuthModule.NAME)
+public class BiometricAuthModule extends ReactContextBaseJavaModule {
+  public static final String NAME = "BiometricAuth";
+
+  public BiometricAuthModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+  }
+
+  @Override
+  @NonNull
+  public String getName() {
+    return NAME;
+  }
+
+
+  // Example method
+  // See https://reactnative.dev/docs/native-modules-android
+  @ReactMethod
+  public void multiply(double a, double b, Promise promise) {
+    promise.resolve(a * b);
+  }
+}
